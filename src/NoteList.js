c055df2733b746fc6a9a503675cb5c80a6d38665
Andrew Oppenheimer <aoppenh@purdue.edu>
2017-06-14 14:50:05 -0400

    changed buttons and removed default list items

--- src/NoteList.js
+++ src/NoteList.js
@@ -3,60 +3,27 @@ import React from 'react'
 import './NoteList.css'
 import './Every.css'
 
+import Data from './NoteForm'
+
 const NoteList = () => {
     return (
         <div className="NoteList">
             <h3>Notes</h3>
+            <button className="fa fa-trash-o" id="clear" />
             <ul id="notes">
                 <li>
                     <div className="note">
                         <div className="note-title">
-                            Citizens of distant epochs
+                            TEMP
                         </div>
                         <div className="note-body">
                             <p>
-                                Sea of Tranquility the ash of stellar alchemy vastness is bearable only through love bits of moving fluff are creatures of the cosmos, consciousness a still more glorious dawn awaits two ghostly white figures in coveralls and helmets are soflty dancing tingling of the spine, concept of the number one brain is the seed of intelligence are creatures of the cosmos?
+                                TEMP
                             </p>
                         </div>
                         <div className="note-buttons">
-                            <button className="fa fa-eraser">
-                            </button>
-                            <button className="fa fa-star-o">
-                            </button>
-                        </div>
-                    </div>
-                </li>
-
-                <li>
-                    <div className="note">
-                        <div className="note-title">Preserve and cherish that pale blue dot </div>
-                        <div className="note-body">
-                            <p>
-                                network of wormholes a billion trillion the only home we've ever known light years dream of the mind's eye. Intelligent beings!
-                            </p>
-                        </div>
-                        <div className="note-buttons">
-                            <button className="fa fa-eraser">
-                            </button>
-                            <button className="fa fa-star-o">
-                            </button>
-                        </div>
-                    </div>
-                </li>
-
-                <li>
-                    <div className="note">
-                        <div className="note-title">Laws of physics</div>
-                        <div className="note-body">
-                            <p>
-                                Cambrian explosion radio telescope, circumnavigated citizens of distant epochs brain is the seed of intelligence two ghostly white figures in coveralls and helmets are soflty dancing galaxies inconspicuous motes of rock and gas
-                            </p>
-                        </div>
-                        <div className="note-buttons">
-                            <button className="fa fa-eraser">
-                            </button>
-                            <button className="fa fa-star-o">
-                            </button>
+                            <button className="fa fa-eraser" id="del" />
+                            <button className="fa fa-star-o" id="fav" />
                         </div>
                     </div>
                 </li>
