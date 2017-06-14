2b34fb2f5c9aa5461a208997fe965f7b15188c15
Andrew Oppenheimer <aoppenh@purdue.edu>
2017-06-14 14:15:59 -0400

    fixed error

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -10,7 +10,7 @@ const Sidebar = () => {
             <div className="logo">
                 <img src={quill} alt="Noteherder" />
             </div>
-            <button className="new-note" onClick={}>
+            <button className="new-note">
                 <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1152887/new-hover.png" alt="New note" />
                 <img className="outline" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1152887/new.png" alt="New note" />
             </button>
