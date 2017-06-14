4a067dd0b55ca7b9d2aeedffa2278dc10904f39d
Andrew Oppenheimer <aoppenh@purdue.edu>
2017-06-14 14:40:03 -0400

    added fav and del buttons, but do nothing

--- src/NoteList.js
+++ src/NoteList.js
@@ -18,8 +18,10 @@ const NoteList = () => {
                                 Sea of Tranquility the ash of stellar alchemy vastness is bearable only through love bits of moving fluff are creatures of the cosmos, consciousness a still more glorious dawn awaits two ghostly white figures in coveralls and helmets are soflty dancing tingling of the spine, concept of the number one brain is the seed of intelligence are creatures of the cosmos?
                             </p>
                         </div>
-                        <div className="note-delete">
+                        <div className="note-buttons">
                             <button className="fa fa-eraser">
+                            </button>
+                            <button className="fa fa-star-o">
                             </button>
                         </div>
                     </div>
@@ -33,6 +35,12 @@ const NoteList = () => {
                                 network of wormholes a billion trillion the only home we've ever known light years dream of the mind's eye. Intelligent beings!
                             </p>
                         </div>
+                        <div className="note-buttons">
+                            <button className="fa fa-eraser">
+                            </button>
+                            <button className="fa fa-star-o">
+                            </button>
+                        </div>
                     </div>
                 </li>
 
@@ -43,6 +51,12 @@ const NoteList = () => {
                             <p>
                                 Cambrian explosion radio telescope, circumnavigated citizens of distant epochs brain is the seed of intelligence two ghostly white figures in coveralls and helmets are soflty dancing galaxies inconspicuous motes of rock and gas
                             </p>
+                        </div>
+                        <div className="note-buttons">
+                            <button className="fa fa-eraser">
+                            </button>
+                            <button className="fa fa-star-o">
+                            </button>
                         </div>
                     </div>
                 </li>
